eabcb8892cda7d67255c0be8f45bcc78346bc8f1
sofiafaddi <faddisofiaa@gmail.com>
2017-12-16 12:04:56 +0100

    Ajoutez des fichiers projet.

---
+++ Test/Test/MasterPageMaster.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Test
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MasterPageMaster : ContentPage
+    {
+        public ListView ListView;
+
+        public MasterPageMaster()
+        {
+            InitializeComponent();
+
+            BindingContext = new MasterPageMasterViewModel();
+            ListView = MenuItemsListView;
+        }
+
+        class MasterPageMasterViewModel : INotifyPropertyChanged
+        {
+            public ObservableCollection<MasterPageMenuItem> MenuItems { get; set; }
+            
+            public MasterPageMasterViewModel()
+            {
+                MenuItems = new ObservableCollection<MasterPageMenuItem>(new[]
+                {
+                    new MasterPageMenuItem { Id = 0, Title = "Accueil", Icon ="@drawable/image.png", TargetType=typeof(MainPage)},
+                    new MasterPageMenuItem { Id = 1, Title = "Partenaire", Icon ="@drawable/image.png", TargetType=typeof(MainPage) },
+                    new MasterPageMenuItem { Id = 2, Title = "Gestion et administration", Icon ="@drawable/image.png", TargetType=typeof(MainPage) },
+                    new MasterPageMenuItem { Id = 3, Title = "Nous contacter", Icon ="@drawable/image.png" , TargetType=typeof(MainPage)},
+                });
+            }
+            
+            #region INotifyPropertyChanged Implementation
+            public event PropertyChangedEventHandler PropertyChanged;
+            void OnPropertyChanged([CallerMemberName] string propertyName = "")
+            {
+                if (PropertyChanged == null)
+                    return;
+
+                PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+            #endregion
+        }
+    }
+}
---
+++ Test/Test/MasterPageMenuItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Test
+{
+
+    public class MasterPageMenuItem
+    {
+        public MasterPageMenuItem()
+        {
+            TargetType = typeof(MasterPageDetail);
+        }
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public String Icon { get;  set; }
+        public Type TargetType { get; set; }
+    }
+}
